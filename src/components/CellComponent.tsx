cf24c38dd0ca54e99086dbccbc9d5bd3b13fe2bc
lovetraindriver <lovetraindriver@gmail.com>
2022-06-21 17:05:24 +0700

    figures logic

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -2,11 +2,16 @@ import React, { FC } from "react";
 import { Cell } from "../models/Cell";
 
 interface CellProps {
-    cell: Cell
+    cell: Cell;
+    selected: boolean;
+    click: (cell: Cell) => void;
 }
 
-const CellComponent: FC<CellProps> = ({cell}) => {
-    return <div className={`cell ${cell.color}`}>
+const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
+    return <div 
+    className={`cell ${cell.color} ${selected ? 'selected' : ''} ${cell.figure && cell.available ? 'active' : ''}`}
+    onClick={()=>{click(cell)}}>
+        {!cell.figure && cell.available && <div className="available"></div>}
         {cell.figure?.logo ? <img src={cell.figure.logo}/> : ''}
         </div>
 }
--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -20,4 +20,73 @@ export class Cell {
         this.available = false;
         this.id = Math.random()
     }
+
+    isEmpty() {
+        return this.figure === null;
+    }
+
+    isEmptyVertical(target:Cell):boolean {
+        if(this.x !== target.x) {
+            return false
+        }
+
+        const min = Math.min(this.y, target.y),
+            max = Math.max(this.y, target.y);
+
+        for (let i = min+1; i < max; i++) {
+            if (!this.board.getCell(this.x, i).isEmpty()){
+                return false
+            }
+        }
+        return true
+    }
+
+    isEmptyHorizontal(target:Cell):boolean {
+        if(this.y !== target.y) {
+            return false
+        }
+
+        const min = Math.min(this.x, target.x),
+            max = Math.max(this.x, target.x);
+
+        for (let i = min+1; i < max; i++) {
+            if (!this.board.getCell(i, this.y).isEmpty()){
+                return false
+            }
+        }
+        return true
+    }
+
+    isEmptyDiagonal(target:Cell):boolean {
+        const absX = Math.abs(target.x - this.x),
+            absY = Math.abs(target.y - this.y)
+
+        if (absY !== absX) {
+            return false
+        }
+
+        const dy = this.y < target.y ? 1 : -1,
+            dx = this.x < target.x ? 1 : -1
+
+        for (let i = 1; i < absY; i++) {
+            if(!this.board.getCell(this.x + dx * i, this.y + dy * i).isEmpty()) {
+                return false;
+            }
+        }
+
+        return true
+    }
+
+    setFigure(figure: Figure) {
+        this.figure = figure;
+        this.figure.cell = this;
+    }
+
+    moveFigure(target: Cell) {
+        if (this.figure?.canMove(target)){
+            this.figure?.moveFigure(target);
+            target.setFigure(this.figure);
+            this.figure = null;
+        }
+    }
 }
